6e94bc156274b47339ed684f0371018b05ac2946
Kavish Singla <singlakavish09@gmail.com>
2025-02-08 18:49:02 +0530

    project added

---
+++ index/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btnSuccess = document.querySelector(".btn-success");
+    
+    btnSuccess.addEventListener("click", function() {
+        alert("Redirecting to Credit Karma!");
+    });
+});
